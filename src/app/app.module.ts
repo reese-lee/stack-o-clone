38129772f8b546116de1137d5bbdefbde4525ed6
heather zurek and reese lee <heather@>
2019-06-18 14:42:17 -0700

    add sidebar, main middle, right bar components, and login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MiddleComponent } from './middle/middle.component';
+import { RightbarComponent } from './rightbar/rightbar.component';
+import { LoginComponent } from './login/login.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthenticationComponent
+    AuthenticationComponent,
+    SidebarComponent,
+    MiddleComponent,
+    RightbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
